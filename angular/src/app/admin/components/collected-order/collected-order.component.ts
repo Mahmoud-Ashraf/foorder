ebd057e166f21bef8e01c35c91a0dabded2e5268
Mahmoud taha <mahmoud.taha@idemia.com>
2022-04-23 07:01:00 +0200

    Enhance collected order page

--- angular/src/app/admin/components/collected-order/collected-order.component.ts
+++ angular/src/app/admin/components/collected-order/collected-order.component.ts
@@ -1,3 +1,4 @@
+import { ResturantsService } from 'src/app/shared/services/resturants.service';
 import { Order } from './../../../models/order';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
@@ -9,7 +10,7 @@ import { OrderService } from 'src/app/shared/services/order.service';
   styleUrls: ['./collected-order.component.scss']
 })
 export class CollectedOrderComponent implements OnInit {
-  resturantId: any;
+  resturant: any;
   orders: {
     orders: Order[],
     collectedOrderPrice: number,
@@ -23,37 +24,35 @@ export class CollectedOrderComponent implements OnInit {
 
   constructor(
     private orderService: OrderService,
-    private authService: AuthService
+    private resturantsService: ResturantsService,
   ) { }
 
   ngOnInit(): void {
-    // this.orders.collectedOrderPrice = 0;
-    this.resturantId = localStorage.getItem('toDayResturantId');
-    this.getTodayOrders();
-    // this.authService.getAuthUserListner().subscribe((user: any) => {
-    //   this.getuserOrders()
-    // })
+    this.getTodayResturant();
   }
 
   getTodayOrders() {
-    if (this.resturantId) {
-      this.orderService.getTodayOrders(this.resturantId).subscribe((todayOrders: any) => {
+    if (this.resturant._id) {
+      this.orderService.getTodayOrders(this.resturant._id).subscribe((todayOrders: any) => {
         console.log(todayOrders);
         this.orders = todayOrders;
         this.getCollectedOrderPrice();
-        // const uniqeOrder = new Set(this.orders.items);
-        // console.log(this.orders);
       });
     }
   }
 
   getCollectedOrderPrice() {
-    // this.orders.collectedOrderPrice = 
     let total = 0;
-    // let initialValue = 0
     this.orders.collectedOrderPrice = this.orders.orders.reduce(
       (previousValue: any, currentValue: any) => previousValue + currentValue.totalOrderPrice
       , total
     )
+  }
+
+  getTodayResturant() {
+    this.resturantsService.getTodayResturant().subscribe(toDayResturant => {
+      this.resturant = toDayResturant;
+      this.getTodayOrders();
+    })
   }
 }

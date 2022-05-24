6818d01b05c76b2d508e415bc4249e7ed5d515fd
Mahmoud taha <mahmoud.taha@idemia.com>
2022-05-24 10:33:27 +0200

    make today order & food orders get data from backend

--- angular/src/app/admin/components/orders/food-orders/food-orders.component.ts
+++ angular/src/app/admin/components/orders/food-orders/food-orders.component.ts
@@ -1,3 +1,5 @@
+import { Resturant } from './../../../../models/resturant';
+import { ResturantsService } from './../../../../shared/services/resturants.service';
 import { OrderService } from './../../../../shared/services/order.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,13 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FoodOrdersComponent implements OnInit {
   ordersHistory: any;
+  todayResturant: Resturant;
+  now = new Date();
   constructor(
-    private orderService: OrderService
+    private orderService: OrderService,
+    private resturantsService: ResturantsService
   ) { }
 
   ngOnInit(): void {
+    this.resturantsService.getTodayResturant().subscribe(todayResturant => {
+      this.todayResturant = todayResturant;
+    })
     this.orderService.getCollectedOrders().subscribe(collectedOrders => {
-      this.ordersHistory = collectedOrders.collectedOrders;
+      this.ordersHistory = collectedOrders.collectedOrders.filter((order: any) => order.status === 'DONE');
     })
   }
 

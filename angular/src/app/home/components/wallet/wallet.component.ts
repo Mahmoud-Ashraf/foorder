243aba36c0d49445bbcfedf067958544abaf071e
Mahmoud taha <mahmoud.taha@idemia.com>
2022-06-13 17:46:42 +0200

    user wallet page

--- angular/src/app/home/components/wallet/wallet.component.ts
+++ angular/src/app/home/components/wallet/wallet.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-wallet',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wallet.component.scss']
 })
 export class WalletComponent implements OnInit {
-
-  constructor() { }
+  private userListnerSub: Subscription;
+  loggedUser: any;
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
+    this.loggedUser = this.authService.getLoggedUser();
+    this.userListnerSub = this.authService
+    .getAuthUserListner()
+    .subscribe((user: any) => {
+      this.loggedUser = user;
+      // console.log('loggedUser: ', this.loggedUser);
+    })
   }
 
 }

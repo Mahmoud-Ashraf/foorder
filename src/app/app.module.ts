c5d6e1af9a6ac544bb14786def96b9494f51d27c
Mahmoud taha <mahmoud.taha@idemia.com>
2022-06-14 16:54:41 +0200

    remove flash messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { MenuService } from './shared/services/menu.service';
 import { AuthService } from './shared/services/auth.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FlashMessagesModule } from 'flash-messages-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -25,7 +24,6 @@ import { ResturantsService } from './shared/services/resturants.service';
     AppRoutingModule,
     QuicklinkModule,
     SharedModule,
-    FlashMessagesModule.forRoot(),
     HttpClientModule
   ],
   bootstrap: [AppComponent],
